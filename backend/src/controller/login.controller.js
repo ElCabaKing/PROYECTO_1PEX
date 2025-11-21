b66cf7ba984f86deede50fd75a72f0409841dac4
CabaKing <davi.cabanilla@gmail.com>
2025-11-20 20:59:21 -0500

    The login was completed at last

--- backend/src/controller/login.controller.js
+++ backend/src/controller/login.controller.js
@@ -1,12 +1,14 @@
 import jwt from 'jsonwebtoken'
 import modelLogin from "../model/login.model.js";
+import bcrypt from 'bcrypt'
 
 export const ctLogin = async (req, res) => {
-    console.log(req.body.user_nombre)
+    const saltRounds = 10;
     try {
         const user_id_password = await modelLogin.mdLogin(req.body.user_nombre);
         if (!user_id_password) { return res.json({ login: false }) }
-        if (req.body.user_password === user_id_password.user_password) {
+        const valid = await bcrypt.compare(req.body.user_password , user_id_password.user_password);
+        if (valid) {
             const payload = { id: user_id_password.id, user_nombre: req.body.user_nombre, isLoged: true }
             const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" });
             res.cookie("auth_token", token, {
