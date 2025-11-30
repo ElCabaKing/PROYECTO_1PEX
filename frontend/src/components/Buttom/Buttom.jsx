69b3cb9ae588182d13744e65fa4deb409c00055e
CabaKing <davi.cabanilla@gmail.com>
2025-11-30 17:13:08 -0500

    last conflic

--- frontend/src/components/Buttom/Buttom.jsx
+++ frontend/src/components/Buttom/Buttom.jsx
@@ -1,14 +1,10 @@
 import styles from "./Buttom.module.css"
 function Buttom(props) {
   return (
-<<<<<<< HEAD
-    <button className={styles.Buttom_base} onClick={props.action}>{props.label}</button>
-=======
     <button 
     type={props.type} 
     className={styles.Buttom_base} 
     onClick={props.action}>{props.label}</button>
->>>>>>> ft/MainMenu
   )
 }
 
